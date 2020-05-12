ad447b72192d33a8e9398f489381b71a39bb4d99
soleperson <h2magic@126.com>
2020-05-12 21:36:14 +0800

    login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+    <div>
+        Login
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+
 </style>
