111aaf383861642d4404851a81c176353025ecb1
soleperson <h2magic@126.com>
2020-05-12 22:49:29 +0800

    Login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,49 +1,81 @@
 <template>
-    <div class="login_container">
-        <div class="login_box">
-            <div class="avatar_box">
-                <img src="../assets/logo.png" alt="">
-            </div>
-        </div>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+
+      <el-form :model="LoginForm" label-width="0px" class="login_form">
+        <el-form-item>
+          <el-input v-model="LoginForm.username" prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="LoginForm.password" prefix-icon="el-icon-lock" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+           <el-button type="primary">登陆</el-button>
+           <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      LoginForm: {
+        username: 'soleperson',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
 <style lang='less' scoped>
-.login_container{
-    background-color: #2b4b6b;
-    height: 100%;
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
 }
-.login_box{
-    width: 450px;
-    height: 300px;
-    background-color: #ffffff;
-    border-radius: 3px;
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eeeeee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #dddddd;
     position: absolute;
     left: 50%;
-    top: 50%;
     transform: translate(-50%, -50%);
-    .avatar_box{
-        height: 130px;
-        width: 130px;
-        border: 1px solid #eeeeee;
-        border-radius: 50%;
-        padding: 10px;
-        box-shadow: 0 0 10px #dddddd;
-        position: absolute;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background-color: #ffffff;
-        img{
-            width: 100%;
-            height: 100%;
-            border-radius: 50%;
-            background-color: #eeeeee;
-        }
+    background-color: #ffffff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eeeeee;
     }
+  }
+}
+.btns{
+    display: flex;
+    justify-content: flex-end;
+}
+.login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
 }
 </style>
